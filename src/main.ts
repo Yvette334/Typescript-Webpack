77a9d4068449e1c4627c42159f2787041ae7002a
Yvette334 <y.muhoracye@alustudent.com>
2026-09-03 17:18:34 +0200

    Add addReview function with type annotations and data

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,17 @@ interface Review {
     comment: string;
 }
 
+const reviews: Review[] = [];
+
+function addReview(bookId: number, rating: number, comment: string): void {
+    const newReview: Review = {bookId, rating, comment};
+    reviews.push(newReview);
+    console.log("Added the review", newReview);
+}
+
+addReview(1, 5, "Is a perfect, timeless adventure");
+addReview(2, 4, " Epic masterpiece that sets the gold standard for fantasy");
+
 // Create some book objects
 const book1: Book = new Book("The Hobbit", "J.R.R. Tolkien", 1937);
 const book2: Book = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954);
